aaff75f3feee16bd0bc42862b66d7a4cab94f796
Gamecrafter <gamecraftervt@gmail.com>
2016-07-23 17:04:21 -0700

    Some small fixes

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,7 +1,7 @@
-var points = {
+var data = {
     c: {value: 0, human: "C"},
-    cpp: {value: 0, human: "C++"}, //C++
-    cs: {value: 0, human: "C#"}, //C#
+    cpp: {value: 0, human: "C++"},
+    cs: {value: 0, human: "C#"},
     html: {value: 0, human: "HTML"},
     java: {value: 0, human: "Java"},
     javascript: {value: 0, human: "JavaScript"},
@@ -9,18 +9,22 @@ var points = {
     php: {value: 0, human: "PHP"},
     python: {value: 0, human: "Python"},
     ruby: {value: 0, human: "Ruby"},
-    scratch: {value: 0, human: "Scratch"}, //You appear to be new to programming, it'd be best if you start by using Scratch
+    scratch: {value: 0, human: "Scratch"},
     swift: {value: 0, human: "Swift"}
 };
 //Page stuff
 $(document).ready(function(){
     $(".choice").click(function(){
-        //console.log(points);
         var $twoUp = $(this).parent().parent();
         var $actions = $(this).attr("action").trim().toLowerCase().split(",");
         //Process user choice
         for(var $key in $actions){
-            points[$actions[$key]]["value"] += 1;
+            //Tiebreaker, in case several languages have the same value points, this one will add more points to only one
+            if($twoUp.attr("id") === "q15"){
+                data[$actions[$key]]["value"] += 3;
+                break;
+            }
+            data[$actions[$key]]["value"] += 1;
         }
         if($twoUp.attr("id") === "q1"){
             $("#header").delay(300).addClass("animated slideOutUp").fadeOut();
@@ -29,14 +33,19 @@ $(document).ready(function(){
         $twoUp.delay(500).addClass("animated fadeOutUp").fadeOut();
         $twoUp.next().delay(1600).addClass("animated fadeInUp").fadeIn();
         if($twoUp.is(":last-child")){
-            var best = findMax(points);
+            var best = findMax(data);
             $("#result-image").attr("src", "images/logo/"+best+".png");
-            $("#result-human").html(points[best]["human"]);
+            $("#result-human").html(data[best]["human"]);
             $("#result-why").attr("href", "why.html#"+best);
-            $("#result")
-                .append("<a href='https://twitter.com/intent/tweet?text="+escape(points[best]["human"]+" is my programming language. What's yours?")+"&url=https%3A%2F%2Fifvictr.github.io%2Ffypl&hashtags=code,"+best+"' target='_blank'><i class='fa fa-twitter fa-4x'></i></a>")
-                .delay(1500).addClass("animated slideInUp").fadeIn();
+            $("#action-twitter").attr("href", "https://twitter.com/intent/tweet?text="+escape(data[best]["human"]+" is my programming language. What's yours?")+"&url=https%3A%2F%2Fifvictr.github.io%2Ffypl&hashtags=code,"+best);
+            $("#result").delay(1500).addClass("animated slideInUp").fadeIn();
         }
+        /*
+        console.log("On question #"+$twoUp.attr("id").substring(1));
+        for(var key in data){
+            console.log(data[key]["human"]+": "+data[key]["value"]);
+        }
+        */
     });
     $(".language").hover(
         function(){
